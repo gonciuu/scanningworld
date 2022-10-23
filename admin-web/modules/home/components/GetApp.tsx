7c2b9cc909da041e3ce17ae03dcc6458eae7c713
kriziu <brunodzi07@gmail.com>
2022-10-23 18:34:17 +0200

    Add links to home page

--- admin-web/modules/home/components/GetApp.tsx
+++ admin-web/modules/home/components/GetApp.tsx
@@ -3,20 +3,30 @@ import { FaGooglePlay, FaApple } from 'react-icons/fa';
 const GetApp = () => {
   return (
     <div className="flex items-center justify-center gap-10">
-      <div className="flex h-16 w-52 items-center justify-between rounded-lg bg-black p-4 text-white">
+      <a
+        className="flex h-16 w-52 items-center justify-between rounded-lg bg-black p-4 text-left text-white hover:bg-black/90 active:bg-black"
+        href="https://github.com/gonciuu/scanningworld"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FaGooglePlay className="h-10 w-10" />
         <div>
           <p className="text-xs">GET IT ON</p>
           <p className="text-xl font-semibold leading-none">Google Play</p>
         </div>
-      </div>
-      <div className="flex h-16 w-52 items-center justify-between rounded-lg bg-black p-6 text-white">
+      </a>
+      <a
+        className="flex h-16 w-52 items-center justify-between rounded-lg bg-black p-6 text-left text-white hover:bg-black/90 active:bg-black"
+        href="https://github.com/gonciuu/scanningworld"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FaApple className="h-10 w-10" />
         <div>
           <p className="text-xs">Download on the</p>
           <p className="text-xl font-semibold leading-none">App Store</p>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
